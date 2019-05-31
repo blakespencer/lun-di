669e9977d38fbb1b39dc9cf5710ca72d71f55847
Blake Spencer <blake.spencer1@gmail.com>
2019-05-31 08:52:48 -0400

    [feature] created a server

--- src/components/navbar/NavBarRight.js
+++ src/components/navbar/NavBarRight.js
@@ -7,11 +7,19 @@ export default class NavBarRight extends Component {
       <React.Fragment>
         <MediaQuery query="(min-device-width: 1224px)">
           <ul>
-            <li>About</li>
-            <li>UK</li>
-            <li>GBP</li>
-            <li>
+            <li className="nav-hover">About</li>
+            <li className="nav-hover">UK</li>
+            <li className="nav-hover">GBP</li>
+            <li className="nav-hover" id="nav-user">
               <i class="far fa-user" />
+              {/* <hr /> */}
+              <span id="user-popup" className="toolbox-user">
+                <ul>
+                  <li>Login</li>
+                  <li>My Profile</li>
+                  <li>My Orders</li>
+                </ul>
+              </span>
             </li>
             <li>
               <i class="fas fa-shopping-cart" />
